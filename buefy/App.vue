91baa9205c77abb10343082d73bb554496bf2b4c
Zemian Deng <zemian.deng@oracle.com>
2020-09-29 12:29:27 -0400

    Add buefy

---
+++ buefy/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="section">
+		<h1 class="title">Buefy UI Component Tester</h1>
+		<div>
+			<UiTest></UiTest>
+		</div>
+	</div>
+</template>
+
+<script>
+// This main App.vue will load other component for testing
+
+import UiTest from './UiTest'
+
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
+export default {
+	components: {
+		UiTest
+	}
+}
+</script>
---
+++ buefy/readme.md
@@ -0,0 +1,5 @@
+Exploring [Buefy](https://buefy.org/) = Bulma + VueJS
+
+Try `hello.html` to see an example, or run `npx vue serve App.vue`
+
+To explore more [UI components](https://buefy.org/documentation), copy example in their site and then paste it into `UiTest.vue` file, and then `npx vue serve App.vue` should auto reload it.
